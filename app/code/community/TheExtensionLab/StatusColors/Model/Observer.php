502878d2c93b8bcd128713347627a4a606ab298e
James Anelay <jamesanelay@gmail.com>
2015-01-17 16:38:35 +0000

    Add system config

--- app/code/community/TheExtensionLab/StatusColors/Model/Observer.php
+++ app/code/community/TheExtensionLab/StatusColors/Model/Observer.php
@@ -34,4 +34,42 @@
             }
         }
     }
+
+    public function controllerActionPredispatchAdminhtmlSystemConfigEdit(Varien_Event_Observer $observer)
+    {
+        $section = $observer->getEvent()->getControllerAction()->getRequest()->getParam('section');
+
+        switch($section){
+            case "theextensionlab_statuscolors":
+                if(!$this->_isSectionAllowed($section)) {
+                    //Credit to @schmengler for making our idea easy to implement
+                    //https://github.com/schmengler/AclReload
+                    $session = Mage::getSingleton('admin/session');
+                    $session->setAcl(Mage::getResourceModel('admin/acl')->loadAcl());
+                }
+                break;
+        }
+    }
+
+
+    protected function _isSectionAllowed($section)
+    {
+        try {
+            $session = Mage::getSingleton('admin/session');
+            $resourceLookup = "admin/system/config/{$section}";
+            if ($session->getData('acl') instanceof Mage_Admin_Model_Acl) {
+                $resourceId = $session->getData('acl')->get($resourceLookup)->getResourceId();
+                if (!$session->isAllowed($resourceId)) {
+                    throw new Exception('');
+                }
+                return true;
+            }
+        }
+        catch (Zend_Acl_Exception $e) {
+            return false;
+        }
+        catch (Exception $e) {
+            return false;
+        }
+    }
 }
